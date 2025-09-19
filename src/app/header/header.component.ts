2a6c39d736e69030a99f3c45474dd42801a83de6
Flavia Kosti <flaviakosti@gmail.com>
2025-09-19 15:10:58 -0400

    AngularV16-Final-Project: Re-design P1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,10 +4,11 @@ import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService) { }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
